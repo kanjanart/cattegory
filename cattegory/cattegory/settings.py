b43907630c79917f78cc34d990aecf4fd049f01a
Kwang Kanjanart <kwang@prontomarketing.com>
2018-09-11 23:43:12 +0700

    Sort order of INSTALL_APPS :imp:

--- cattegory/cattegory/settings.py
+++ cattegory/cattegory/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'detail.apps.DetailConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'detail.apps.DetailConfig',
 ]
 
 MIDDLEWARE = [
