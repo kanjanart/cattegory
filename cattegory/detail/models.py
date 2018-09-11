08b865ee28ad6adbefaaa9d3c0f13474fd1fbf07
Kwang Kanjanart <kwang@prontomarketing.com>
2018-09-11 23:59:05 +0700

    Delete comment and make class to camel :imp:

--- cattegory/detail/models.py
+++ cattegory/detail/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-# Create your models here.
-class cattegory(models.Model):
+
+class Cattegory(models.Model):
     species = models.CharField(max_length=50)
     price = models.IntegerField()
     food = models.CharField(max_length=50)
