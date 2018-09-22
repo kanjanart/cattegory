679fd50a88e01726e18eb2f817c0df3a7487188d
Kwang Kanjanart <kwang@prontomarketing.com>
2018-09-23 00:38:10 +0700

    Add template and display template view :imp:

--- cattegory/detail/views.py
+++ cattegory/detail/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from detail.models import Cattegorys
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello cattegory")
+    list_species = []
+    get_all_cattegory = Cattegorys.objects.all()
+
+    for cattegory in get_all_cattegory:
+        species = cattegory.species
+        list_species.append(species)
+
+    template = loader.get_template('index.html')
+    context = {'list_species': list_species}
+
+    return HttpResponse(template.render(context, request))
