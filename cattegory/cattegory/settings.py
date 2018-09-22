ec3ac770800f79ff7ec0d8d5638da283fde06421
Kwang Kanjanart <kwang@prontomarketing.com>
2018-09-23 00:32:45 +0700

    Add template in setting :imp:

--- cattegory/cattegory/settings.py
+++ cattegory/cattegory/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'cattegory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/detail/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
