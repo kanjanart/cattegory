37ef84cb75d0392b4536a619d8c3870b157734bd
Kwang Kanjanart <kwang@prontomarketing.com>
2018-09-23 00:33:54 +0700

    Edit integer field :imp:

--- cattegory/detail/models.py
+++ cattegory/detail/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Cattegorys(models.Model):
     species = models.CharField(null=True, blank=True, max_length=50)
-    price = models.IntegerField(null=True, blank=False, max_length=10)
+    price = models.IntegerField(null=True, blank=False)
     food = models.CharField(null=True, blank=True, max_length=50)
     buy_place = models.CharField(null=True, blank=True, max_length=50)
